c3a09b67c168dda58999514e0768f17fe38191d7
yasminnelm <yassamineelm11@gmail.com>
2024-11-23 12:47:18 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',  // Make sure jsdom is used to simulate a browser environment
+    globals: true,          // This allows global usage of test, expect, and vi
+    setupFiles: './setupTests.js',  // Point to your setup file
+    reporters: [
+      'default',
+      ['junit', { suiteName: 'UI tests' }],
+      'lcov',
+    ],
+    coverage: {
+      reporter: ['lcov', 'text', 'json'], // 'lcov' generates the report in a format SonarQube can read
+      all: true,
+      exclude: ['node_modules/**', 'tests/**'], 
+    },
+  }
+})
